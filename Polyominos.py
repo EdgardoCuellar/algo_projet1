8690e3bac4a8a416d301ef7beaeb053ffe247bb5
Edgardo610 <Edgardo610@users.noreply.github.com>
2020-02-27 15:40:12 +0100

    Backtracking it's hard hehe

--- Polyominos.py
+++ Polyominos.py
@@ -11,6 +11,7 @@ class Ma_matrice:
         La matrice doit être recentrée lors de l'initialisation de l'objet en utilisant translation_haut_gauche(self)"""
         self.matrice = liste
         self.size = len(liste)
+        self.translation_haut_gauche()
 
     def colonne(self, col_index):
         """ Input: col_index est un int
@@ -28,7 +29,7 @@ class Ma_matrice:
         new_m = []
         for pos_x in range(self.size):
             line = []
-            for pos_y in range(self.size-1, -1, -1):
+            for pos_y in range(self.size - 1, -1, -1):
                 line.append(self.matrice[pos_y][pos_x])
             new_m.append(line)
         return Ma_matrice(new_m)
@@ -48,8 +49,8 @@ class Ma_matrice:
         Pas de output"""
         for _ in range(n):
             for pos_y in range(self.size):
-                if pos_y != self.size-1:
-                    self.matrice[pos_y] = self.matrice[pos_y+1]
+                if pos_y != self.size - 1:
+                    self.matrice[pos_y] = self.matrice[pos_y + 1]
                 else:
                     self.matrice[pos_y] = [0] * self.size
 
@@ -59,8 +60,8 @@ class Ma_matrice:
         for _ in range(n):
             for pos_y in range(self.size):
                 for pos_x in range(self.size):
-                    if pos_x != self.size-1:
-                        self.matrice[pos_y][pos_x] = self.matrice[pos_y][pos_x+1]
+                    if pos_x != self.size - 1:
+                        self.matrice[pos_y][pos_x] = self.matrice[pos_y][pos_x + 1]
                     else:
                         self.matrice[pos_y][pos_x] = 0
 
@@ -74,7 +75,7 @@ class Ma_matrice:
         nb_up = 0
         s_up = True
         for line in self.matrice:
-            if s_up and line == [0]*self.size:
+            if s_up and line == [0] * self.size:
                 nb_up += 1
             else:
                 s_up = False
@@ -84,12 +85,11 @@ class Ma_matrice:
         nb_l = 0
         s_l = True
         for pos_x in range(self.size):
-            if s_l and self.colonne(pos_x) == [0]*self.size:
+            if s_l and self.colonne(pos_x) == [0] * self.size:
                 nb_l += 1
             else:
                 s_l = False
         self.translation_gauche(nb_l)
-
 
 
 class Piece:
@@ -102,7 +102,7 @@ class Piece:
         Toutes ses rotations/réflexions différentes doivent être ajoutées dans la
         liste self.liste_variantes"""
         self.nom = nom
-        self.liste_variantes = variante
+        self.liste_variantes = [variante]
 
     def ajout_variante(self, nouvelle_variante):
         """Ajoute l'objet nouvelle_variante de la classe Ma_matrice à la liste self.liste_variantes
@@ -148,7 +148,7 @@ class Tableau:
         Output = True si la pièce a été ajoutée, False si l'ajout n'était pas possible"""
         test_add = True
         pos_list = []
-        for pos_y, line in enumerate(self.liste_pieces[index_piece].liste_variantes.matrice):
+        for pos_y, line in enumerate(self.liste_pieces[index_piece].liste_variantes[num_var].matrice):
             for pos_x, case in enumerate(line):
                 if self.tableau[pos_y + pos[0]][pos_x + pos[1]] == " " and case == 1:
                     pos_list.append((pos_x + pos[1], pos_y + pos[0]))
@@ -159,6 +159,7 @@ class Tableau:
             self.Pos_pieces[index_piece] = pos
             for pos_case in pos_list:
                 self.tableau[pos_case[1]][pos_case[0]] = self.liste_pieces[index_piece].nom
+        return test_add
 
     def enlever_piece(self, index_piece):
         """Enlève la pièce à la position index_piece dans self.liste_piece du tableau.
@@ -168,7 +169,7 @@ class Tableau:
         Si la pièce n'est pas sur le tableau, cette fonction ne fait rien.
         Pas de output"""
         pos = self.Pos_pieces[index_piece]
-        for pos_y, line in enumerate(self.liste_pieces[index_piece].liste_variantes.matrice):
+        for pos_y, line in enumerate(self.liste_pieces[index_piece].liste_variantes[0].matrice):
             for pos_x, case in enumerate(line):
                 if self.tableau[pos_y + pos[0]][pos_x + pos[1]] != " " and case == 1:
                     self.tableau[pos_y + pos[0]][pos_x + pos[1]] = " "
@@ -190,7 +191,9 @@ class Tableau:
         profondeur est un integer qui indique la profondeur dans le backtracking qui dans ce cas
         correspond à l'index de la pièce à ajouter à ce niveau du backtracking
         Output = True si une solution trouvée"""
-        pass
+        soluce = False
+
+        return soluce
 
 
 def lire_fichier(nom_fichier):
@@ -198,51 +201,67 @@ def lire_fichier(nom_fichier):
     Output = liste d'objets de classe Piece représentant les polyominos
     contenus dans le fichier"""
     liste_pieces = []
-    with open(nom_fichier + '.poly', 'r') as file:
+    with open(nom_fichier, 'r') as file:
         str_file = file.read().split("\n")
     nb_p = int(str_file[0][0])
     size = int(str_file[0][2])
     for where in range(nb_p):
-        name = str_file[(where*(size+1))+1][0]
+        name = str_file[(where * (size + 1)) + 1][0]
         matrice = []
-        for pos_y in range(2, size+2):
+        for pos_y in range(2, size + 2):
             line = []
-            for pos_x in range(0, size*2, 2):
-                line.append(str_file[(where*(size+1))+pos_y][pos_x])
+            for pos_x in range(0, size * 2, 2):
+                line.append(int(str_file[(where * (size + 1)) + pos_y][pos_x]))
             matrice.append(line)
-        liste_pieces.append(Piece(name, matrice))
+        liste_pieces.append(Piece(name, Ma_matrice(matrice)))
     return liste_pieces
+
 
 def taille_totale_pieces(liste_pieces):
     """ Input = liste d'objet de classe Piece
     Output = somme de la taille de chaque pièce"""
     nb = 0
     for piece in liste_pieces:
-        for line in piece.liste_variantes.matrice:
+        for line in piece.liste_variantes[0].matrice:
             nb += line.count(1)
     return nb
+
 
 def possibles_factorisations(nb):
     """Input: nb est un int
     Output = liste de tuples de taille 2 contenant toutes les factorisations possibles en
     2 facteurs de nb. Attention à ne pas les répéter 2 fois."""
     facto = []
-    for i in range(1, int(nb/2)):
+    for i in range(1, int(nb / 2)):
         for j in range(nb, 0, -1):
             if i <= j and i * j == nb:
                 facto.append((i, j))
     return facto
 
+
 def trouver_liste_solutions(nom_fichier):
     """Fonction principale qui trouve la liste des solutions.
     Output = liste d'objets de la classe Tableau qui contiennent les différentes solutions"""
     pieces = lire_fichier(nom_fichier)
+    nb_piece = taille_totale_pieces(pieces)
+    nb_add = 0
     solutions = []
+    soluce = False
+
+    while not soluce:
+        factos = possibles_factorisations(nb_piece + nb_add)
+        for size in factos:
+            tab = Tableau(size, pieces)
+            if tab.backtracking(1):
+                solutions.append(tab)
+                soluce = True
+        nb_add += 1
 
     return solutions
 
+
 if __name__ == '__main__':
-    nom = lire_fichier("set_pieces_1")
+    noms = trouver_liste_solutions("set_pieces_2.poly")
     # nom_fichier = sys.argv[1]
     # liste_solutions = trouver_liste_solutions(nom_fichier)
     # for tableau in liste_solutions:
